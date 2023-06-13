5f9ac89b6501bee39c64faeb22a07cb83e00e796
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-06-14 00:54:47 +0900

    init

---
+++ netflix/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
+import App from './app';
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
