5f13b4d93b14be5dca1d1b5d812f37aeab692f6a
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-06-24 18:12:07 +0900

    Update firebase.prod.js

--- netflix-master/src/lib/firebase.prod.js
+++ netflix-master/src/lib/firebase.prod.js
@@ -6,13 +6,13 @@ import 'firebase/auth';
 import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: "AIzaSyBktEhBgUWyE51tCNliviGHwq60qj9Sk9E",
-  authDomain: "hk-netflix.firebaseapp.com",
-  projectId: "hk-netflix",
-  storageBucket: "hk-netflix.appspot.com",
-  messagingSenderId: "720837488979",
-  appId: "1:720837488979:web:5b96c1d122a27fc4bab1a6",
-  measurementId: "G-3B9F3KQRC6"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId ,
+  storageBucket: process.env.REACT_APP_storageBucket ,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId ,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
 
 const firebase = Firebase.initializeApp(config);
