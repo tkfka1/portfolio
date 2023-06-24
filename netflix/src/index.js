02a650fd1546e0b64266872b9064e157846639ec
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-06-24 18:10:02 +0900

    624

--- netflix/src/index.js
+++ netflix/src/index.js
@@ -3,11 +3,15 @@ import App from './app';
 import 'normalize.css';
 import { createRoot } from 'react-dom/client';
 import { GlobalStyles } from './global-styles';
+import { firebase } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
 
 const rootElement = document.getElementById('root');
 createRoot(rootElement).render(
     <>
+    <FirebaseContext.Provider value={{ firebase }}>
         <GlobalStyles />
-        <App />, 
+        <App />
+    </FirebaseContext.Provider>
     </>
 );
--- netflix/src/lib/firebase.prod.js
+++ netflix/src/lib/firebase.prod.js
@@ -1,23 +1,21 @@
-import Firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import { initializeApp } from "firebase/app";
 
 // 1) when seeding the database you'll have to uncomment this!
-import { seedDatabase } from '../seed';
+// import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: "AIzaSyBktEhBgUWyE51tCNliviGHwq60qj9Sk9E",
-  authDomain: "hk-netflix.firebaseapp.com",
-  projectId: "hk-netflix",
-  storageBucket: "hk-netflix.appspot.com",
-  messagingSenderId: "720837488979",
-  appId: "1:720837488979:web:5b96c1d122a27fc4bab1a6",
-  measurementId: "G-3B9F3KQRC6"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId ,
+  storageBucket: process.env.REACT_APP_storageBucket ,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId ,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
 
-const firebase = Firebase.initializeApp(config);
+const firebase = initializeApp(config);
 // 2) when seeding the database you'll have to uncomment this!
-seedDatabase(firebase);
+// seedDatabase(firebase);
 // 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
 
 export { firebase };
@@ -35,13 +33,13 @@ export { firebase };
 // // Your web app's Firebase configuration
 // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // const firebaseConfig = {
-//   apiKey: "AIzaSyBktEhBgUWyE51tCNliviGHwq60qj9Sk9E",
-//   authDomain: "hk-netflix.firebaseapp.com",
-//   projectId: "hk-netflix",
-//   storageBucket: "hk-netflix.appspot.com",
-//   messagingSenderId: "720837488979",
-//   appId: "1:720837488979:web:5b96c1d122a27fc4bab1a6",
-//   measurementId: "G-3B9F3KQRC6"
+//   apiKey: "",
+//   authDomain: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: "",
+//   measurementId: ""
 // };
 
 // // Initialize Firebase
