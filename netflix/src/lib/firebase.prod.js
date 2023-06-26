1220deb117486acf23552ba47b2ed2155ca28638
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-06-26 18:48:44 +0900

    update

--- netflix/src/lib/firebase.prod.js
+++ netflix/src/lib/firebase.prod.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 // import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId ,
-  storageBucket: process.env.REACT_APP_storageBucket ,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId ,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId
+  apiKey: "AIzaSyBktEhBgUWyE51tCNliviGHwq60qj9Sk9E",
+  authDomain: "hk-netflix.firebaseapp.com",
+  projectId: "hk-netflix" ,
+  storageBucket: "hk-netflix.appspot.com" ,
+  messagingSenderId: "720837488979" ,
+  appId: "1:720837488979:web:5b96c1d122a27fc4bab1a6",
+  measurementId: "G-3B9F3KQRC6"
 };
 
 const firebase = initializeApp(config);
--- netflix/src/lib/firebase.prod_cop.js
+++ netflix/src/lib/firebase.prod_cop.js
@@ -6,13 +6,13 @@ import 'firebase/auth';
 // import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId ,
+  storageBucket: process.env.REACT_APP_storageBucket ,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId ,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
 
 const firebase = Firebase.initializeApp(config);
